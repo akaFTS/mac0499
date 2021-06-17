33559d8432a507b5afad3ed1f7f1d6eea023c2fd
akaFTS <gustavohfts1@gmail.com>
2021-06-17 00:12:54 +0000

    Initial attempt at adding Tachyons

--- app.tsx
+++ app.tsx
@@ -1,12 +1,17 @@
-import React, { ComponentType } from 'react'
+import React, { ComponentType } from "react";
+import Header from "~/components/layout/header.jsx";
+import "https://raw.githubusercontent.com/tachyons-css/tachyons/master/css/tachyons.min.css";
 
-export default function App({ Page, pageProps }: { Page: ComponentType<any>, pageProps: any }) {
+export default function App(
+  { Page, pageProps }: { Page: ComponentType<any>; pageProps: any },
+) {
   return (
     <main>
       <head>
         <meta name="viewport" content="width=device-width" />
       </head>
+      <Header />
       <Page {...pageProps} />
     </main>
-  )
+  );
 }
